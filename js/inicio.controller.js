188b98b28ed50426f9a3d070395b9f48ae208e50
Hugo Luis Santiago Altamirano <hugo.santiago@iebo.edu.mx>
2018-06-04 12:29:17 -0500

    Especificaciones en cámara

--- js/inicio.controller.js
+++ js/inicio.controller.js
@@ -212,6 +212,7 @@ $(document).ready(function(){
 
 function initScan(){
 	cordova.plugins.barcodeScanner.scan(function(result){
+		if (result)
 		if (codigosScaneados.indexOf(result.text) == -1){
 			console.log("Código escaneado");
 			db.transaction(function(tx){
@@ -243,6 +244,10 @@ function initScan(){
 		}		
 	},function(error){
 		showCodigos();
+	}, {
+		disabledSuccessBeep: false,
+		prompt: "Scanea tu código",
+		preferFrontCamera: true
 	});
 }
 
